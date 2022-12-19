2fd25634ebebc3169fa87ed6dc210db3451707b8
hoangnv170752 <hoang.nv.ral@gmail.com>
2022-12-19 14:34:21 +0700

    khai bao bien

---
+++ todolist/lib/main.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String name = "Hoang";
+  int age = 20;
+  bool old = false;
+  double time = 0;
+
+  name = name + " Nguyen";
+  //moi kieu gia tri 
+  dynamic all;
+  all = "Hello";
+  all = 20;
+  //var la khai bao mang kieu cua kieu duoc gan gia tri mac dinh
+  print(all);
+}
