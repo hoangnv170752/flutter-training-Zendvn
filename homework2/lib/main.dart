6a0621f338de7eaee00aeddecfc67ab41b7be0be
hoangnv170752 <hoang.nv.ral@gmail.com>
2023-05-18 21:27:14 +0700

    nho sua loi grid view

--- homework2/lib/main.dart
+++ homework2/lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:homework2/pages/BT01.dart';
 import 'package:homework2/pages/BT02.dart';
+import 'package:homework2/pages/BT03.dart';
 
 void main(List<String> args) {
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: BT02(),
+    home: BT03(),
   ));
 }
---
+++ homework2/lib/pages/BT03.dart
@@ -0,0 +1,173 @@
+import 'package:flutter/material.dart';
+
+class BT03 extends StatelessWidget {
+  const BT03({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        elevation: 0,
+        backgroundColor: Colors.white,
+        title: Text(
+          "Traning",
+          style: TextStyle(
+              fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    flex: 3,
+                    child: Text(
+                      'Vel et voluotatiiu',
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Detail',
+                        style: TextStyle(
+                            color: Colors.yellow,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15),
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Icon(
+                        Icons.arrow_forward,
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Container(
+                padding: EdgeInsets.all(20),
+                margin: EdgeInsets.symmetric(vertical: 20),
+                width: double.infinity,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.amber,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(70),
+                    bottomLeft: Radius.circular(70),
+                  ),
+                ),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            "debitis-ipsa-ut",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                          const SizedBox(height: 10),
+                          const Text(
+                            "Soluta sed possimus libero omnis alias qui Soluta sed possimus libero omnis alias qui knckkascbkscbk.",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.login,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                'ZendVn',
+                                style: TextStyle(color: Colors.white),
+                              )
+                            ],
+                          ),
+                          Container(
+                            height: 30,
+                            width: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.horizontal(
+                                left: Radius.circular(20),
+                                right: Radius.circular(20),
+                              ),
+                              color: Colors.white,
+                            ),
+                            child: Icon(
+                              Icons.settings,
+                              color: Colors.amber,
+                            ),
+                          )
+                        ],
+                      ),
+                    ]),
+              ),
+              Container(
+                width: double.infinity,
+                height: 70,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [
+                      Colors.amber,
+                      Colors.white,
+                      Colors.amber,
+                    ],
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(5),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              const Text("Quia voluptatum culpa"),
+              // GridView(
+              //   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              //     crossAxisCount: 2,
+              //     crossAxisSpacing: 10,
+              //     mainAxisSpacing: 10,
+              //   ),
+              //   children: List.generate(
+              //     20,
+              //     (index) => Container(
+              //       color: Colors.amber,
+              //       height: 300,
+              //       width: 200,
+              //     ),
+              //   ),
+              // )
+              //   ],
+              // ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
